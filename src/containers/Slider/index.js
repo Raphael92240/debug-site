5ac729d9533250b4a9b14efd4d79cf4565ee78be
samuel <samuel92240@gmail.com>
2023-10-25 16:12:08 +0200

    final

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
 
@@ -8,52 +8,41 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const timeoutRef = useRef(null);
-
-  const delay = 5000;
+  const [autoScrollPaused, setAutoScrollPaused] = useState(false);
 
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtB.date) - new Date(evtA.date)
   );
 
+
+
+  // Fonction pour afficher la carte suivante
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
+    if (byDateDesc && byDateDesc.length > 0 && !autoScrollPaused) {
+      setIndex((prevIndex) =>
+        prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0
+      );
+    }
   };
 
-
-
-  function resetTimeout() {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-  }
-
-
+  // Appel de la fonction nextCard toutes les 5 secondes
   useEffect(() => {
-    resetTimeout();
-    timeoutRef.current = setTimeout(
-      () =>
-        setIndex((prevIndex) =>
-          prevIndex === byDateDesc.length - 1 ? 0 : prevIndex + 1
-        ),
-      delay
-    );
-
+    const timer = setInterval(nextCard, 5000);
     return () => {
-      resetTimeout();
+      clearInterval(timer);
     };
-  }, [index]);
+  }, [index, autoScrollPaused]);
 
+  // Fonction pour mettre en pause le défilement automatique
+  const pauseAutoScroll = () => {
+    setAutoScrollPaused(true);
+  };
 
+  // Fonction pour reprendre le défilement automatique
+  const resumeAutoScroll = () => {
+    setAutoScrollPaused(false);
+  };
 
-
-
-  useEffect(() => {
-    nextCard();
-  }, []);
 
 
   return (
@@ -61,37 +50,37 @@ const Slider = () => {
       {byDateDesc?.map((event, idx) => (
         <React.Fragment key={`${idx + 1}`}>
           <div
-            key={event.title}
             className={`SlideCard SlideCard--${index === idx ? "display" : "hide"
               }`}
+            onMouseEnter={pauseAutoScroll}
+            onMouseLeave={resumeAutoScroll}
+
           >
             <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
+            <div className="SlideCarddescriptionContainer">
+              <div className="SlideCarddescription">
                 <h3>{event.title}</h3>
                 <p>{event.description}</p>
                 <div>{getMonth(new Date(event.date))}</div>
               </div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
+          <div className="SlideCardpaginationContainer">
+            <div className="SlideCardpagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={`${radioIdx + 1}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
-                  onClick={() => {
-                    setIndex(idx);
-                  }}
+                  onChange={() => setIndex(radioIdx)}
+                  checked={index === radioIdx}
                 />
               ))}
             </div>
           </div>
-        </React.Fragment>
+        </React.Fragment >
       ))}
-    </div>
+    </div >
   );
 };
 
